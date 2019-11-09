53245bfc9c66529d6092a210c8ef810ec2fdf05e
hugh <hezhengjie@meituan.com>
2019-11-09 17:33:54 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,7 +33,14 @@ module.exports = {
                 '/chapter2/chapter2.0',
                 '/chapter2/chapter2.1',
                 '/chapter2/chapter2.2',
-                '/chapter2/chapter2.3'
+                '/chapter2/chapter2.3',
+                '/chapter2/chapter2.4',
+                '/chapter2/chapter2.5',
+                '/chapter2/chapter2.6',
+                '/chapter2/chapter2.7',
+                '/chapter2/chapter2.8',
+                '/chapter2/chapter2.9',
+                '/chapter2/chapter2.10'
 
               ]
             }
